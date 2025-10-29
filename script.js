ded55e6a3b5bd957926eb0207e8e9e77a228d3bc
joshuaowm <joshuaowen1500@gmail.com>
2025-10-29 23:36:28 +0100

    Commit
    Major update

--- script.js
+++ script.js
@@ -7,20 +7,39 @@ menu.onclick = () => {
 };
 
 var swiper = new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+    slidesPerView: 2,
+    grid: {
+        rows: 2,
+    fill: "row",
+    },
+    spaceBetween: 20,
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
-},
-navigation: {
+    },
+    navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-},
-autoplay: {
+    },
+    autoplay: {
     delay: 3000,
     disableOnInteraction: false,
+    },
+    speed: 1000,
+    loop: false, // ❌ disable loop to prevent grid issues
+    breakpoints: {
+    0: {
+    slidesPerView: 1,
+    grid: {
+        rows: 1,
+    },
+    },
+    768: {
+    slidesPerView: 2,
+    grid: {
+        rows: 2,
+    },
+    },
 },
-speed: 1700,
 });
+
